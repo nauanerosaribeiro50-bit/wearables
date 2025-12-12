34b97c8b1d9f32e644494f4471ef2da944ca3afc
Nauane Ribeiro <nauanerosaribeiro50@gmail.com>
2025-12-12 14:30:53 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showContent(id) {
-  document.querySelectorAll(".content").forEach(el => el.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
-}
-
-function goHome() {
-  document.querySelectorAll(".content").forEach(el => el.classList.remove("active"));
-  document.getElementById("home").classList.add("active");
-}
